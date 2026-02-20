53a7e69670f6411c4e50842e5c7e9e7ab2a5bb5b
gialli24 <a.gialli2409@gmail.com>
2026-02-20 15:22:44 +0100

    Add snack 01

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,3 +3,13 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
+let namesMessage = "";
+names.forEach((name, index) => {
+    namesMessage += `'${name}'`
+
+    if (index !== names.length - 1) {
+        namesMessage += `, `
+    }
+})
+
+console.log(namesMessage);
